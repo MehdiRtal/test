2eb2d24ca6c09da455722d141529be09ce66bf19
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2025-05-14 09:52:39 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,12 +4,12 @@ import pulumi
 provider = k8s.Provider("k8s", render_yaml_to_directory="yaml")
 
 k8s.core.v1.Pod(
-    "nginx-2",
+    "nginx-3",
     metadata=k8s.meta.v1.ObjectMetaArgs(name="nginx", namespace="default"),
     spec=k8s.core.v1.PodSpecArgs(
         containers=[
             k8s.core.v1.ContainerArgs(
-                name="nginx",
+                name="nginx-3",
                 image="nginx:latest",
                 ports=[k8s.core.v1.ContainerPortArgs(container_port=80)],
             )
