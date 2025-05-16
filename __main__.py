35525ae54535dd0d07e2bdcbd68f649e460b863b
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2025-05-16 07:13:02 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,13 +6,13 @@ config = pulumi.Config()
 provider = k8s.Provider("k8s", render_yaml_to_directory="yaml")
 
 k8s.core.v1.Pod(
-    config.get("pod-name", "nginx"),
+    config.require("pod-name", "nginx"),
     metadata=k8s.meta.v1.ObjectMetaArgs(name="nginx", namespace="default"),
     spec=k8s.core.v1.PodSpecArgs(
         containers=[
             k8s.core.v1.ContainerArgs(
-                name=config.get("container-name", "nginx"),
-                image=config.get("image-name", "nginx"),
+                name=config.require("container-name", "nginx"),
+                image=config.require("image-name", "nginx"),
                 ports=[k8s.core.v1.ContainerPortArgs(container_port=80)],
             )
         ]
