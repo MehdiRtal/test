83624de9aa0f58a085fda2a3d12419e6a9653a90
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2025-05-17 03:46:54 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ provider = k8s.Provider("k8s", render_yaml_to_directory="yaml")
 
 k8s.core.v1.Pod(
     config.get("pod-name", "nginx"),
-    metadata=k8s.meta.v1.ObjectMetaArgs(name="nginx", namespace="default"),
+    metadata=k8s.meta.v1.ObjectMetaArgs(name="nginx", namespace="default", annotations=config.get_object("annotations", None)),
     spec=k8s.core.v1.PodSpecArgs(
         containers=[
             k8s.core.v1.ContainerArgs(
